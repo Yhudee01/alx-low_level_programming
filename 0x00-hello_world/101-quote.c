828643a831f06d699d208906797c691b4d9983c1
Yhudee01 <clarryjay26@gmail.com>
2022-09-08 19:40:10 -0700

    Quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-#include <stdio.h>
+#include <unistd.h>
 /**
- *main - entry point
- *
- *Description: prints and that piece..
- *Return: 1 if sucessful
- */
+*main - entry point
+*
+*Description: prints and that piece..
+*Return: 1 if successful
+*/
 int main(void)
 {
-  write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\
 n",59);
-  return (1);
+return (1);
 }
