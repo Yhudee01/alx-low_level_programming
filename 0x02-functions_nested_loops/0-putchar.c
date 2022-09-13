1e992d788af5cf9cf4b40c521a62b22be99f3e42
Yhudee01 <clarryjay26@gmail.com>
2022-09-13 16:50:02 -0700

    Putc

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
-* main - Entry point
+* main - Print _putchar as a message
 *
 * Return: Always 0 (success)
 */
 int main(void)
 {
-char *sh = "_putchar";
-
-while (*sh)
+int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+int count, sz;
+  
+sz = sizeof(str) / sizeof(int);
+for (count = 0; count < sz; count++)
 {
-putchar(*sh);
-sh++;
+_putchar(str[count]);
 }
-putchar('\n')
-
+_putchar('\n')
 return (0);
 }
