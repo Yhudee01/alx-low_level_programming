145856155a7cf5d3912b131636ec103bec5e9450
Yhudee01 <clarryjay26@gmail.com>
2022-09-15 12:42:48 -0700

    Had

---
+++ 0x03-debugging/o-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+"c* main - test function that prints if integer is negative or positive
+* Retun: 0
+*/
+int main(void)
+{
+int i;
+
+i = 0;
+positive_or_negative(i);
+
+return (o);
+}
